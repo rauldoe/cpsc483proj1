1f86302b5a996e278d611deedad2f1aa8c85412c
rauldoe <khoado@csu.fullerton.edu>
2019-11-18 13:20:03 -0800

    further updates

--- id3.py
+++ id3.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import numpy as np
 
-
+dependentAttribute = 'outlook'
 df = pd.read_csv('tennis.csv')
 
-data = df.to_numpy()
+rowCount = df.shape[0]
+colCount = df.shape[1]
 
-print(data)
+data = df.to_numpy()
 
 attribList = df.axes[1]
 
@@ -15,4 +16,21 @@ print(len(attribList))
 print(attribList[0])
 print(attribList[1])
 
+rowCount = len(data[0])
 
+dependentList = df[dependentAttribute].to_numpy()
+unique_elements, counts_elements = np.unique(dependentList, return_counts=True)
+countList = np.asarray((unique_elements, counts_elements))
+
+len(df['outlook'])
+
+a = np.array( ['YES', 'NO', 'YES', 'NO', 'YES', 'NO', 'YES'] )
+print("Original array:")
+print(a)
+unique_elements, counts_elements = np.unique(a, return_counts=True)
+print("Frequency of unique values of the said array:")
+print(np.asarray((unique_elements, counts_elements)))
+
+
+def calculateEntropy(attribList, data, dependentAttribute):
+    return 1
