b2113369b8a00354744ce6082afbc2c758dc5fc4
Khoa Do <kdo1@costar.com>
2019-11-19 15:35:42 -0800

    information gain

--- id3.py
+++ id3.py
@@ -19,7 +19,7 @@ def getValueCountLookup(dataFrame, attributeName):
 def getFeatureList(attributeList, dependentAttribute):
     return [i for i in attributeList if i != dependentAttribute]
 
-def calculateEntropy(valueCountLookup):
+def entropy(valueCountLookup):
     entropy = 0.0
     lookup = valueCountLookup['lookup']
     total = valueCountLookup['total']
@@ -32,8 +32,50 @@ def calculateEntropy(valueCountLookup):
 def join(arr, delim):
     joined = ''
     for i in range(0, len(arr)):
-        joined = joined + arr[i] + delim
+        joined = joined + str(arr[i]) + delim
     return joined
+
+def toStringList(list):
+    strList = []
+    for i in list:
+        strList.append(str(i))
+    return strList
+
+def unique(matrix):
+    d = {}
+
+    for i in range(0, len(matrix)):
+        listKey = toStringList(matrix[i].tolist())
+        key = join(listKey, '/')
+        if (key in d.keys()):
+            count = int(d[key]['count']) + 1
+            d[key] = {'listkey' : listKey, 'count' : count}
+        else:
+            d.update({key : {'listkey' : listKey, 'count' : 1}})
+    return d
+
+def getLookupItem(lookup, attribute):
+    list = []
+    total = 0
+    for i in lookup.keys():
+        if (lookup[i]['listkey'][0] == attribute):
+            list.append(lookup[i])
+            total += int(lookup[i]['count'])
+    return {'list' : list, 'total' : total}
+
+def entropyWrt(lookupItem):
+    entropy = 0.0
+
+    total = int(lookupItem['total'])
+    lookup = lookupItem['list']
+
+    for i in lookup:
+        # attribValue = i['listkey'][0]
+        count = int(i['count'])
+        
+        p = count/total
+        entropy -= p * math.log(p, 2)
+    return entropy
 
 dependentAttribute = 'play'
 
@@ -47,22 +89,33 @@ data = df.to_numpy()
 
 attribList = df.axes[1]
 
+fList = getFeatureList(attribList, dependentAttribute)
+
 k1 = getValueCountLookup(df, dependentAttribute)
-e = calculateEntropy(k1)
-print(e)
+totalEntropy = entropy(k1)
+print(totalEntropy)
+
+for feature in fList:
+    fm = df[feature].to_numpy()
+    featureOutcomes = toStringList(np.unique(fm, return_counts=False))
+    # print(featureOutcomes)
+    m = df[[feature, dependentAttribute]].to_numpy()
+    dict = unique(m)
+
+    outcomeSummation = 0.0
+    for outcome in featureOutcomes:
+        li = getLookupItem(dict, outcome)
+        pOutcome = int(li['total'])/rowCount
+        e = entropyWrt(li)
+        outcomeSummation -= pOutcome * e
+        # print(e)
+
+    informationGain = totalEntropy + outcomeSummation
+    print(feature + ': ' + str(informationGain))
 
 
-m = df[['outlook', 'play']].to_numpy()
-dict = {}
 
-for i in range(0, len(m)):
-    val = m[i]
-    key = join((m[i]).tolist(), '/')
-    if (key in dict.keys()):
-        count = dict[key]['count']
-        dict[key] = count + 1
-    else:
-        dict[key] = 1
-# df[['outlook', 'play']].groupby(['outlook']).count()
 
-print(dict)
+# li = getLookupItem(dict, 'True')
+# e = entropyWrt(li)
+# print(e)
