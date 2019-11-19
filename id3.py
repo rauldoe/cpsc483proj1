f951f09daa0447e5c83195b611030c4dd35bba7e
rauldoe <khoado@csu.fullerton.edu>
2019-11-18 22:44:50 -0800

    calculate entropy

--- id3.py
+++ id3.py
@@ -1,36 +1,48 @@
 
 import pandas as pd
 import numpy as np
+import math
 
-dependentAttribute = 'outlook'
+def getValueCountLookup(dataFrame, attributeName):
+    total = 0
+    keyLookup = {}
+    dependentList = dataFrame[attributeName].to_numpy()
+    keyList, countList = np.unique(dependentList, return_counts=True)
+
+    for i in range(0, len(keyList)):
+        total += countList[i]
+        keyLookup.update({keyList[i] : countList[i]})
+
+    return {'lookup' : keyLookup, 'total' : total}
+
+def getFeatureList(attributeList, dependentAttribute):
+    return [i for i in attributeList if i != dependentAttribute]
+
+def calculateEntropy(valueCountLookup):
+    entropy = 0.0
+    lookup = valueCountLookup['lookup']
+    total = valueCountLookup['total']
+    # print(lookup)
+    # print(total)
+
+    for key in lookup:
+        px = lookup[key]/total
+        entropy -= px * math.log(px, 2)
+    return entropy
+
+dependentAttribute = 'play'
 df = pd.read_csv('tennis.csv')
 
 rowCount = df.shape[0]
 colCount = df.shape[1]
-
 data = df.to_numpy()
 
 attribList = df.axes[1]
 
-print(len(attribList))
-print(attribList[0])
-print(attribList[1])
+k1 = getValueCountLookup(df, dependentAttribute)
 
-rowCount = len(data[0])
+e = calculateEntropy(k1)
 
-dependentList = df[dependentAttribute].to_numpy()
-unique_elements, counts_elements = np.unique(dependentList, return_counts=True)
-countList = np.asarray((unique_elements, counts_elements))
-
-len(df['outlook'])
-
-a = np.array( ['YES', 'NO', 'YES', 'NO', 'YES', 'NO', 'YES'] )
-print("Original array:")
-print(a)
-unique_elements, counts_elements = np.unique(a, return_counts=True)
-print("Frequency of unique values of the said array:")
-print(np.asarray((unique_elements, counts_elements)))
+print(e)
 
 
-def calculateEntropy(attribList, data, dependentAttribute):
-    return 1
